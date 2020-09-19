7b00947995173ed8dd5185cc9e696903e6f27553
ShakedBu <buch.shaked@gmail.com>
2020-09-19 15:03:56 +0300

    speech page start

--- src/Components/SpeechPage/SpeechPage.js
+++ src/Components/SpeechPage/SpeechPage.js
@@ -1,14 +1,26 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { useParams } from "react-router-dom";
+import { getSpeech } from '../../Utils/SpeechUtil';
 
-// const useStyles = makeStyles((theme) => ({
-// }));
+const useStyles = makeStyles((theme) => ({
+}));
 
 function SpeechPage(props) {
-    //const classes = useStyles();
+    const classes = useStyles();
+
+    const [speech, setSpeech] = useState(null);
+
+    var speechId = useParams();
+
+    if (speech == null) {
+        getSpeech(speechId).then((response) => { setSpeech(response); })
+    }
 
     return (
-        <div></div>
+        <div>
+            {speech && speech.full_text}
+        </div>
     );
 }
 
