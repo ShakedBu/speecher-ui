cc14063e50fbd298b91a318a2c056faf3d527073
ShakedBu <buch.shaked@gmail.com>
2020-09-22 18:30:54 +0300

    fix speech view \n

--- src/Components/SpeechPage/SpeechView.js
+++ src/Components/SpeechPage/SpeechView.js
@@ -11,10 +11,10 @@ function SpeechView(props) {
 
     return (
         <>
-            <Typography variant="h2" component="h3">{props.speech && props.speech.name}</Typography>
-            <Typography variant="h5" component="h6">{props.speech && props.speech.speaker + " | " + props.speech.location + " | " + props.speech.date}</Typography>
-            <Paper style={{ maxHeight: 800, overflow: 'auto' }} >
-                {props.speech && props.speech.full_text}
+            <Typography variant="h2" component="h3">{props.speech?.name}</Typography>
+            <Typography variant="h5" component="h6">{props.speech?.speaker + " | " + props.speech?.location + " | " + props.speech?.date}</Typography>
+            <Paper style={{ maxHeight: 800, overflow: 'auto', textAlign: 'left' }} >
+                {props.speech?.full_text.split('\n\n').map((x) => <><div>{x}</div><br /></>)}
             </Paper>
         </>
     );
