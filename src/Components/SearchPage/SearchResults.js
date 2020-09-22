dfb95409de25eb06c57eb7b057456104c36a22fc
ShakedBu <buch.shaked@gmail.com>
2020-09-22 18:21:13 +0300

    no max value

--- src/Components/SearchPage/SearchResults.js
+++ src/Components/SearchPage/SearchResults.js
@@ -10,7 +10,6 @@ import { Link } from "react-router-dom";
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-    maxWidth: '36ch',
     backgroundColor: theme.palette.background.paper,
   },
   inline: {
