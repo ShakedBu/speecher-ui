cb0ff7e6e99138ae2313341c1b42985d81501837
ShakedBu <buch.shaked@gmail.com>
2020-09-27 15:33:32 +0300

    fix search page

--- src/Components/SearchPage/SearchPage.js
+++ src/Components/SearchPage/SearchPage.js
@@ -1,22 +1,24 @@
 import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
 import Button from "@material-ui/core/Button";
 import SearchIcon from "@material-ui/icons/Search";
 import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
 import InputBase from '@material-ui/core/InputBase';
 import Divider from '@material-ui/core/Divider';
 import SearchReaults from './SearchResults';
-import { searchSpeeches } from '../../Utils/SpeechUtil';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+
 import NewSpeechPage from '../NewSpeechPage/NewSpeechPage';
+import { searchSpeeches } from '../../Utils/SpeechUtil';
+
 
 const useStyles = makeStyles((theme) => ({
     root: {
         padding: '2px 4px',
         display: 'flex',
-        alignItems: 'center',
-        width: 400,
+        width: 500,
         margin: '10px auto',
     },
     input: {
@@ -69,18 +71,17 @@ function SearchPage() {
 
     return (
         <>
-            <Paper className={classes.root} elevation={3}>
-                <form onSubmit={handleSubmit}>
-                    <InputBase
-                        className={classes.input}
-                        placeholder="Search..."
-                        value={searchedVal}
-                        onChange={(event) => setSearchedVal(event.target.value)}
-                    />
-                    <Button type="search" className={classes.iconButton} aria-label="search" onClick={() => searchSpeech(searchedVal)}>
-                        <SearchIcon />
-                    </Button>
-                </form>
+            <Paper component="form" className={classes.root} onSubmit={handleSubmit}>
+                <InputBase
+                    className={classes.input}
+                    placeholder="Search..."
+                    value={searchedVal}
+                    onChange={(event) => setSearchedVal(event.target.value)}
+                />
+                <Divider className={classes.divider} orientation="vertical" />
+                <Button type="search" className={classes.iconButton} aria-label="search" onClick={() => searchSpeech(searchedVal)}>
+                    <SearchIcon />
+                </Button>
             </Paper>
             <Divider variant="middle" />
             <SearchReaults searchResults={results} query={query} />
